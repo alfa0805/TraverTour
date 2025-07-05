78606d52f9610694083ff03f9f2758ed3d557ac4
alfa0805 <asadbekarslonbekov53@gmail.com>
2025-07-05 10:48:20 +0500

    loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react";
 import { Header, Main } from "./components";
 import { useTranslation } from "react-i18next";
-import i18n from "./i18n";
+import { RingLoader } from "react-spinners";
+
 function App() {
   const { t, i18n } = useTranslation();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!i18n.isInitialized || loading) {
+    return (
+      <div className="flex items-center justify-center h-screen bg-[#000000]">
+        <RingLoader color="#00bfff" size={140} />
+      </div>
+    );
+  }
+
   return (
     <div className="scroll-smooth relative">
-      <header className="">
+      <header>
         <Header />
       </header>
       <main>
